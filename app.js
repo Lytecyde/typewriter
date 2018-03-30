144415cf7f9d8e248f5420be6f9a838ea4c53f76
Tanel Lebedev <tanel.lebedev@gmail.com>
2018-03-30 21:13:24 +0300

    Fix JS linter warnings, dont hide spaces, change text.

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ window.onload = function () {
 	window.sound = new Audio("typeclick.wav");
 
 	window.onkeypress = function (e) {
-		var letter = e.key;  
-		var span = $("span.hidden:contains(" + letter + "):first");
+		var letter = e.key,
+			span = $("span.hidden:contains(" + letter + "):first");
 		span.removeClass("hidden");
 		window.sound.play();
-	}
+	};
 
 	$("p").each(function () {
 	    var p = $(this), 
 	    	text = p.text(),
-	    	spanned = "";
+	    	spanned = "",
+	    	i,
+	    	c;
 
-	    for (var i = 0; i < text.length; i++) {
-	    	var c = text[i];
-	    	spanned = spanned + '<span class="hidden">' + c + "</span>";
+	    for (i = 0; i < text.length; i++) {
+	    	c = text[i];
+	    	if (c !== " ") {
+	    		spanned = spanned + '<span class="hidden">' + c + "</span>";
+	    	} else {
+	    		spanned = spanned + '<span>' + c + "</span>";
+	    	}
 	    }
 
 	    p.html(spanned);
+	    p.removeClass('hidden');
 	});
-}
+};
